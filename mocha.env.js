52ad23d3010834977d45289c7ec0c1d103e9f432
RoboPhred <robophred@gmail.com>
2018-03-25 14:00:29 -0700

    Set up mocha tests; watch compilation and testing.

---
+++ mocha.env.js
@@ -0,0 +1,4 @@
+require("source-map-support").install({
+    // Stop source-map-support killing mocha.
+    handleUncaughtExceptions: false
+});
---
+++ src/binding-impl.spec.ts
@@ -0,0 +1,82 @@
+
+import { expect } from "chai";
+import { describe } from "mocha";
+import * as sinon from "sinon";
+
+import {
+    Context
+} from "./interfaces";
+
+import {
+    ConstBindingImpl, ScopedBindingImpl
+} from "./binding-impl";
+import { Container } from ".";
+
+describe("bindings", function () {
+
+    let context: Context;
+    beforeEach(function () {
+        context = {
+            container: sinon.mock() as any
+        };
+    });
+
+    describe("@ScopedBindingImpl", function () {
+        function testTransientBoundValues() {
+            it("generates a new value on each call", function () {
+                const valueCreator = sinon.stub();
+
+                const subject = new ScopedBindingImpl(valueCreator);
+
+                for (let i = 0; i < 3; i++) {
+                    const expectedValue = `expected-value-${i}`;
+                    valueCreator.onCall(i).returns(expectedValue);
+
+                    const result = subject._getBoundValue(context);
+
+                    expect(result).to.equal(expectedValue);
+                }
+            });
+        }
+
+        describe("unconfigured", function () {
+            describe("#_getBoundValue", function () {
+                testTransientBoundValues();
+            });
+        });
+
+        describe("with #inTransientScope", function () {
+            describe("#_getBoundValue", function () {
+                testTransientBoundValues();
+            });
+        });
+
+        describe("with #inSingletonScope", function () {
+            it("#_getBoundValue", function () {
+                const expectedValue = "expected-value-singleton";
+                const valueCreator = sinon.stub().returns(expectedValue);
+
+                const subject = new ScopedBindingImpl(valueCreator);
+
+                for (let i = 0; i < 3; i++) {
+                    const result = subject._getBoundValue(context);
+                    expect(result).to.equal(expectedValue);
+                }
+            });
+        });
+    });
+
+    describe("@ConstBindingImpl", function () {
+        describe("#_getBoundValue", function () {
+            it("returns the constant value", function () {
+                const expectedValue = "expected-value-constant";
+
+                const subject = new ConstBindingImpl(expectedValue);
+
+                const result = subject._getBoundValue(context);
+
+                expect(result).to.equal(expectedValue);
+            });
+        });
+    });
+});
